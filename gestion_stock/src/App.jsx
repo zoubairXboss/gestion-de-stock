e4e400f183085ae67148f549be5d1f70c36d00bd
walidDev <walidelhafiane.dev@gmail.com>
2025-12-14 19:23:53 +0100

    Sprint 2: Delete product and LocalStorage

--- gestion_stock/src/App.jsx
+++ gestion_stock/src/App.jsx
@@ -1,19 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ProductForm from "./components/ProductForm";
 import ProductList from "./components/ProductList";
 
 function App() {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState(
+    JSON.parse(localStorage.getItem("products")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("products", JSON.stringify(products));
+  }, [products]);
 
   const addProduct = (product) => {
     setProducts([...products, product]);
+  };
+
+  const deleteProduct = (id) => {
+    setProducts(products.filter(p => p.id !== id));
   };
 
   return (
     <div>
       <h1>Gestion de Stock</h1>
       <ProductForm addProduct={addProduct} />
-      <ProductList products={products} />
+      <ProductList products={products} deleteProduct={deleteProduct} />
     </div>
   );
 }
